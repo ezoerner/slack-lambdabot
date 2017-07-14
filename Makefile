e60f05bad2c52061fbe2d96b361a917a307d9a2f
Eric Zoerner <eric.zoerner@nike.com>
2017-07-13 17:16:28 -0700

    ISSUE-001 Support Slack code blocks, both inline and multiline

--- Makefile
+++ Makefile
@@ -2,15 +2,15 @@
 IMAGE_NAME ?= slack-lambdabot:local
 LB_CLI_IMAGE ?= ezoerner/lambdabot-cli:latest
 
-all: setup build
+all: build
 
 setup:
 	docker pull $(LB_CLI_IMAGE)
 
-build:
+build: setup
 	docker build -t=$(IMAGE_NAME) .
 
-run: .check-token
+run: .check-token build
 	docker run --env SLACK_API_TOKEN=$(API_TOKEN) -t --rm $(IMAGE_NAME)
 
 publish:
--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,68 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main (main) where
+module Main (main, parseCommand, parseWord) where
 
 import Codec.Binary.UTF8.String (decodeString)
+import Control.Applicative ((<|>))
 import Control.Monad.IO.Class (liftIO)
 import Data.Functor (void)
 import Data.Functor.Identity (Identity(Identity))
 import Data.Maybe (fromMaybe)
 import Data.Monoid ((<>))
-import Data.Text (Text, dropWhile, pack, unpack, stripPrefix)
+import Data.Text (Text, pack)
 import Data.Text.Lazy (toStrict)
 import Data.Text.Lazy.Builder (toLazyText)
 import HTMLEntities.Decoder (htmlEncodedText)
 import Lambdabot.Main
 import Modules (modulesInfo)
-import Prelude hiding (dropWhile, filter)
+import Prelude hiding (filter)
 import System.Environment (lookupEnv)
 import System.IO.Silently (capture)
-import Web.Slack (Event(Message), Slack, SlackBot, SlackConfig(..), runBot)
+import Text.Parsec (anyChar, between, char, eof, letter, many, many1, noneOf,
+  parse, spaces, string, try)
+import Text.Parsec.Text (Parser)
+import Web.Slack (Event(Message), SlackBot, SlackConfig(..), runBot)
 import Web.Slack.Message (sendMessage)
+
+-- | Parse a command.
+parseCommand :: Parser String
+parseCommand
+  =  try $ spaces
+    *> parsePrefix
+    *> parseContent
+    <* spaces
+    <* eof
+
+parsePrefix :: Parser Char
+parsePrefix =  char '!'
+
+parseContent :: Parser String
+parseContent = 
+  parseWord
+    >>= \cmd -> spaces
+    >> parseCode
+    >>= \code -> return (if null code then cmd else cmd ++ " " ++ code)
+
+parseWord :: Parser String
+parseWord = many1 letter 
+
+-- | Attempt to parse a code block surrounded in @```@. If that does not
+-- work, attempt to parse inline code surrounded in @`@. Finally, if that
+-- does not work, just treat the whole string as code.
+parseCode :: Parser String
+parseCode = try parseCodeBlock <|> try parseInlineCode <|> anyString
+
+-- | Parses a code block surrounded in @```@.
+parseCodeBlock :: Parser String
+parseCodeBlock = between (string "```") (string "```") (many $ noneOf "`")
+
+-- | Parses inline code surrounded in @`@.
+parseInlineCode :: Parser String
+parseInlineCode = between (char '`') (char '`') (many $ noneOf "`")
+
+-- | Parse any string.
+anyString :: Parser String
+anyString = many anyChar
 
 -------------------------------------------------------------------------------
 -- Lambdabot
@@ -53,19 +92,14 @@ envMkSlackConfig key
 mkSlackConfig :: String -> SlackConfig
 mkSlackConfig apiToken = SlackConfig { _slackApiToken = apiToken }
 
-getCommand :: Text -> Slack a (Maybe Text)
-getCommand  message = return $ dropWhile (\c -> c == ':' || c == ' ')
-        <$> stripPrefix "!" message
-
 -- | Construct a @SlackBot@ from a name. This bot will pass messages addressed
 -- to it to 'lambdabot' and relay 'lambdabot''s response.
 slackBot :: SlackBot a
 slackBot (Message cid _ someMessage _ _ _) = do
-  commandMessage <- getCommand $ decodeHtml someMessage
-  case commandMessage of
-    Nothing -> return ()
-    Just command -> do
-      rawResponse <- liftIO (pack . decodeString <$> lambdabot (unpack command))
+  case parse parseCommand "" (decodeHtml someMessage) of
+    Left _ -> return ()
+    Right command -> do
+      rawResponse <- liftIO (pack . decodeString <$> lambdabot command)
       sendMessage cid $ "```\n" <> rawResponse <> "```"
 slackBot _ = return ()
 
